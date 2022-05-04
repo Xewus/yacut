e5b9c8a58bb95f19b3aef4c4ba143a5d9f49dc23
Xewus <xewuss@yandex.ru>
2022-05-04 19:19:16 +0300

    0.3.1 docs errors

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -4,19 +4,43 @@ from . import app
 
 
 class APIException(Exception):
+    """Обработчик ошибок для эндпоинтов /api/* .
+    """
     def __init__(self, message, status_code=400):
         super().__init__()
         self.message = message
         self.status_code = status_code
 
     def as_dict(self):
+        """Формирует сообщение об ошибке в формате словаря.
+
+        Returns:
+            dict: Сообщение об ошибке.
+        """
         return dict(message=self.message)
 
 @app.errorhandler(APIException)
 def api_exception(error):
+    """Обрабатывает ошибки для api.
+
+    Args:
+        error (_type_): _description_
+
+    Returns:
+        tuple(dict, int): Сообщение об ошибке, HTTP-status
+    """
     return jsonify(error.as_dict()), error.status_code
 
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """Обработчик ошибки 404.
+
+    Args:
+        error (exceptions.NotFound): Ошибка.
+
+    Returns:
+        str: html-страница.
+    """
+    print(type(error))
     return render_template('error_pages/404.html'), 404
