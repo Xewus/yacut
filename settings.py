64443d3e7251be3b00e0e5e22bd5e055289dd21e
Xewus <xewuss@yandex.ru>
2022-05-06 16:01:15 +0300

    0.5 getenv

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='lambada')
