c676dd5e4d553b172f22d1ba6b66e3dd18fc4d93
Xewus <xewuss@yandex.ru>
2022-05-07 19:58:07 +0300

    1.0.6

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -22,18 +22,17 @@ def new_short_url():
     Returns:
         Responce: json.
     """
-    original = const.API_REQUEST_FIELDS.original
-    short = const.API_REQUEST_FIELDS.short
     data = request.get_json()
 
     if not data:
         raise APIException(const.NO_REQUEST_BODY)
 
-    if data.get(original) is None:
-        raise APIException(const.NO_REQUIRED_FIELD % original)
+    original = data.get(const.API_REQUEST_FIELDS.original)
 
-    original = data.get(original)
-    short = data.get(short)
+    if original is None:
+        raise APIException(const.NO_REQUIRED_FIELD % const.API_REQUEST_FIELDS.original)
+
+    short = data.get(const.API_REQUEST_FIELDS.short)
 
     if short:
         len_validation(short, APIException(const.API_EXC_MESSAGE), max=const.MAX_LEN_SHORT)
